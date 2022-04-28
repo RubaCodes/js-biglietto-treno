66a15dea226945aad2b49579a5bf7e2c1619de89
rubacodes <rubacodes@gmail.com>
2022-04-28 13:51:03 +0200

    costo per km e cast a number parametri + sconti

--- script/script.js
+++ script/script.js
@@ -1,4 +1,15 @@
 //input distanza 
-const distance = prompt('Inserire la distanza dalla destinazione:');
+const distance = Number(prompt('Inserire la distanza dalla destinazione:'));
 //input age
-const userAge = prompt('Inserire eta del passeggero:');
+const userAge = Number(prompt('Inserire eta del passeggero:'));
+//price per unit
+const pricePerKm = 0.21;
+console.log(userAge, distance);
+
+const overAgeDiscount = 0.4;
+const underAgeDiscout = 0.2;
+
+//costo del biglietto senza sconti
+
+
+
